bb863bf388fec8c5c71963ae5174e3fe881c05ba
JustGithubProject <Kropivadanil@gmail.com>
2022-12-25 19:19:21 +0300

    templates, urls, settings

--- generator/urls.py
+++ generator/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import home
+from .views import home, about, password
 
 urlpatterns = [
-    path('', home, name="home")
+    path('', home, name="home"),
+    path('about/', about, name='about'),
+    path('password/', password, name='password')
 ]
--- generator/views.py
+++ generator/views.py
@@ -4,3 +4,11 @@ from django.shortcuts import HttpResponse
 
 def home(request):
     return render(request, "home.html")
+
+
+def about(request):
+    return render(request, "about.html")
+
+
+def password(request):
+    return render(request, "password.html")
